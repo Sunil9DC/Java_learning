81bced9746d48b54083e11bc5f63723312047528
Sunil DC <84194330+Sunil9DC@users.noreply.github.com>
2026-02-04 22:21:52 +0545

    Add Intro class with main method and loop

---
+++ Intro.java
@@ -0,0 +1,11 @@
+public class Intro {
+    void main() {
+
+        System.out.println(String.format("Hello and welcome!"));
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+
+        }
+    }
